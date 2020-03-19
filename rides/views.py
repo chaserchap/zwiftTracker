c3dc66d5e86b1500b4e2b6e30fe46c650b1dde89
chaserchap <chase.chapman@gmail.com>
2020-03-18 22:06:18 -0500

    Started the home page

--- rides/views.py
+++ rides/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Route
 
 # Create your views here.
+def route_list(request):
+	routes = Route.objects.all()
+	return render(request, 'rides/route_list.html', {'routes': routes})
--- zwifttracker/settings.py
+++ zwifttracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^ff&$gq-asqrk5c8b&rfywcow=usw2*8+*(aff^an10^tqe!8+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
