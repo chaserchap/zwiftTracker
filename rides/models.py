3fdbd0cc3d733bd7de96a95b1b506b929c551ad1
chaserchap <chase.chapman@gmail.com>
2020-03-21 09:03:42 -0500

    Added rides model.

--- rides/models.py
+++ rides/models.py
@@ -15,3 +15,21 @@ class Route(models.Model):
 
     def __unicode__(self):
         return self.name
+
+class Ride(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.SET_DEFAULT, default="Unknown")
+    ride_time = models.FloatField(verbose_name="Total Time")
+    active_time = models.FloatField(verbose_name="Active Time")
+    date = models.DateField()
+    max_power = models.IntegerField(verbose_name="Max Power")
+    avg_power = models.IntegerField(verbose_name="Average Power")
+    max_hr = models.IntegerField(verbose_name="Max Heartrate")
+    avg_hr = models.IntegerField(verbose_name="Average Heartrate")
+    act_dist = models.FloatField(verbose_name="Actual Distance (m)")
+    calories = models.IntegerField()
+    total_work = models.IntegerField(verbose_name="Total Work (kJ)")
+    avg_cadence = models.IntegerField(verbose_name="Average Cadence")
+    max_cadence = models.IntegerField(verbose_name="Max Cadence")
+    act_elevation = models .IntegerField(verbose_name="Actual Elevation Gain (m)")
+    avg_spd = models.FloatField(verbose_name="Average Speed (kph)")
+    max_spd = models.FloatField(verbose_name="Maximum Speed (kph)")
